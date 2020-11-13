8be058279959ab995c795baa6c871bb20ecd933a
HobJob <adria.arroyo@outlook.es>
2020-11-13 19:59:37 +0100

    def

--- puppygram/ViewController.swift
+++ puppygram/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var num: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func segmentedControlChanged(_ sender: UISegmentedControl) {
+        num.textColor = sender.isSelected ? UIColor.black : UIColor.blue
+    }
+    
+    
+    @IBAction func stepperValChanged(_ sender: UIStepper) {
+        num.text = String(Int(sender.value))
+    }
+    
+    /*
+    // MARK: - Navigation
 
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
