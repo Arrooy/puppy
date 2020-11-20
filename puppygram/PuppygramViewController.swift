2da40196ddd4341de506ea94eca3820df30341b2
miquel <miquelsaula@gmail.com>
2020-11-20 20:37:31 +0100

    yata

--- puppygram/PuppygramViewController.swift
+++ puppygram/PuppygramViewController.swift
@@ -11,6 +11,14 @@ class PuppygramViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UINavigationItem!
     
+    @IBOutlet weak var imatgePerfil: UIImageView!
+    @IBOutlet weak var nomPerfil: UILabel!
+    @IBOutlet weak var mainImatge: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var commentButton: UIButton!
+    @IBOutlet weak var nomPerfil2: UILabel!
+    @IBOutlet weak var comment: UILabel!
+    @IBOutlet weak var faXhores: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,10 +29,25 @@ class PuppygramViewController: UIViewController {
 
         UINavigationBar.appearance().titleTextAttributes = attrs
             
+        nomPerfil.font = UIFont.boldSystemFont(ofSize: 16)
+        nomPerfil2.font = UIFont.boldSystemFont(ofSize: 16)
+        
+        faXhores.textColor = UIColor.gray
+        faXhores.font = UIFont.systemFont(ofSize: 12)
+        
+        imatgePerfil.layer.borderWidth = 1
+        imatgePerfil.layer.masksToBounds = false
+        imatgePerfil.layer.borderColor = UIColor.black.cgColor
+        imatgePerfil.layer.cornerRadius = imatgePerfil.frame.height/2
+        imatgePerfil.clipsToBounds = true
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func commentButton(_ sender: Any) {
+    }
+    
+    @IBAction func commentButton(_ sender: Any) {
+    }
     /*
     // MARK: - Navigation
 
